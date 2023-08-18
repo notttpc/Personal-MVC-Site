b3cf17991cf13f23a67744df5b102504c72e763f
Blake Harrison <Blakemharrison@outlook.com>
2023-08-18 16:14:50 -0500

    Add project files.

---
+++ Personal MVC Site/Controllers/HomeController.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Personal_MVC_Site.Models;
+using System.Diagnostics;
+using MimeKit;
+using MailKit.Net.Smtp;
+
+namespace Personal_MVC_Site.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _config;
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Resume()
+        {
+            return View();
+        }
+        public IActionResult Portfolio()
+        {
+            return View();
+        }
+        public IActionResult Link()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel cvm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cvm);
+            }
+
+            #region Email Setup Steps & Email Info
+
+            //1. Go to Tools > NuGet Package Manager > Manage NuGet Packages for Solution
+            //2. Go to the Browse tab and search for NETCore.MailKit
+            //3. Click NETCore.MailKit
+            //4. On the right, check the box next to the CORE1 project, then click "Install"
+            //5. Once installed, return here
+            //6. Add the following using statements & comments:
+            //      - using MimeKit; //Added for access to MimeMessage class
+            //      - using MailKit.Net.Smtp; //Added for access to SmtpClient class
+            //7. Once added, return here to continue coding email functionality
+
+            //MIME - Multipurpose Internet Mail Extensions - Allows email to contain
+            //information other than ASCII, including audio, video, images, and HTML
+
+            //SMTP - Secure Mail Transfer Protocol - An internet protocol (similar to HTTP)
+            //that specializes in the collection & transfer of email data
+
+            #endregion
+
+            string message = $"You have received a new email from your site's contact form!<br />" +
+                $"Sender: {cvm.Name}<br />Email: {cvm.Email}<br />Subject: {cvm.Subject}<br />" +
+                $"Message: {cvm.Message}";
+
+            var mm = new MimeMessage();
+
+            mm.From.Add(new MailboxAddress("Sender", _config.GetValue<string>("Credentials:Email:User")));
+
+            mm.To.Add(new MailboxAddress("Personal", _config.GetValue<string>("Credentials:Email:Recipient")));
+
+            mm.Subject = cvm.Subject;
+
+            mm.Body = new TextPart("HTML") { Text = message };
+
+            mm.Priority = MessagePriority.Urgent;
+
+            mm.ReplyTo.Add(new MailboxAddress("User", cvm.Email));
+
+            using (var client = new SmtpClient())
+            {
+
+                client.Connect(_config.GetValue<string>("Credentials:Email:Client"), 8889);
+
+                client.Authenticate
+                    (
+
+                    //username
+                    _config.GetValue<string>("Credentials:Email:User"),
+
+                    //password
+                    _config.GetValue<string>("Credentials:Email:Password")
+
+                     );
+
+
+                try
+                {
+                    //try to send the email
+                    client.Send(mm);
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.ErrorMessage = $"There was an error processing your request. Please try again later." +
+                        $"<br />Error Message: {ex.StackTrace}";
+
+                    return View(cvm);
+
+                    //throw;
+                }
+
+            }//client is disposed
+
+
+            return View("EmailConfirmation", cvm);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Personal MVC Site
